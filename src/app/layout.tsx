111780f63435dfd6d2f1c9ff61856e31b317b5be
Rishabh Shevde <rishabhshevde@gmail.com>
2026-06-21 19:34:47 +0530

    fix: correct Google Material Symbols @import positioning and add preconnect/stylesheet links to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -68,6 +68,10 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark" suppressHydrationWarning>
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        {/* eslint-disable-next-line @next/next/no-page-custom-font, @next/next/google-font-display */}
+        <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap" rel="stylesheet" />
         <script
           dangerouslySetInnerHTML={{
             __html: `
